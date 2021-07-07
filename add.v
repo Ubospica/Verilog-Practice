af34eca9c23e4250a01000452558a2a4eb201059
Yixin Dong <ubospica@gmail.com>
2021-07-08 00:03:05 +0800

    Update add.v

--- add.v
+++ add.v
@@ -6,7 +6,13 @@ module Add (
     output reg [31:0] sum
 );
     // TO DO
-    // use ^, !, ~, & to implement an 32-bits adder
+    // use ~, !, |, & to implement an 32-bits adder
     // If the digits overflow, discard the overflow digits directly
-
+  reg[31:-1] carry = 0;
+  integer i;
+  always @(*) begin
+    for (i = 0; i < $bits(sum); i = i + 1) begin
+      {carry[i], sum[i]} = a[i] + b[i] + carry[i - 1];
+    end
+  end
 endmodule
